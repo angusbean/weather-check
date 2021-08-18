e86b8148d849e5a53555f92c5598d3850bb3ea10
angusbean <angus.bean.design@gmail.com>
2021-08-18 22:40:55 +1000

    city information being parsed

--- models/models.go
+++ models/models.go
@@ -18,3 +18,18 @@ type Weather struct {
 	} `json:"main"`
 	Code int `json:"cod"`
 }
+
+//CityList provides the struct for the list
+type CityList struct {
+	CityList []City `json:"citylist"`
+}
+
+//City provides struct for city location information
+type City struct {
+	Name  string `json:"name"`
+	Coord struct {
+		Lat  float32 `json:"lat"`
+		Long float32 `json:"lon"`
+	} `json:"coord"`
+	Country string `json:"country"`
+}
--- weather-check.go
+++ weather-check.go
@@ -4,13 +4,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"os"
 
 	"github.com/angusbean/weather-check/models"
-	"github.com/angusbean/weather-check/secrets"
 )
 
 func main() {
+	//Receive Args (Lat and Long) from command line
+	if len(os.Args) != 3 {
+		fmt.Println("error, usage required:", os.Args[0], "Latitude Value", "Longitude Value")
+		os.Exit(1)
+	}
+	lat, long := os.Args[1], os.Args[2]
+	fmt.Println(lat, long)
+
+	// Open the city.list json file and handle erros
+	jsonFile, err := os.Open("openweather-info/city.list.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully opened the JSON file")
+	defer jsonFile.Close()
+
+	// Read opened jsonFile as a byte array
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// Initialise City array
+	var citylist models.CityList
+
+	// Unmarshal byteArray into cities struct
+	json.Unmarshal(byteValue, &citylist)
+
+	// Interate through every city in list
+	for i := 0; i < len(citylist.CityList); i++ {
+		if citylist.CityList[i].Name == "Oxford" {
+			fmt.Println("Found Oxford")
+		}
+	}
+
+	/*s
 	//Recall API Key from secrets
 	APICall := "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=" + secrets.API_key
 
@@ -37,5 +70,5 @@ func main() {
 	var responseObject models.Weather
 	json.Unmarshal(bodyBytes, &responseObject)
 	fmt.Printf("API Response as struct %+v\n", responseObject)
-
+	*/
 }
