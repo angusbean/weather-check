751b1f15065078e3a284f7e9ba2840f8ba3a51fb
angusbean <angus.bean.design@gmail.com>
2021-08-21 00:22:15 +1000

    json post working

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/angusbean/weather-check/config"
+	"github.com/angusbean/weather-check/models"
 )
 
 //Repo the repository used by the handlers
@@ -27,6 +32,33 @@ func NewHandlers(r *Repository) {
 }
 
 //PostLocation accepts JSON input and returns JSON weather information for that location
-func (m *Repository) PostJSON(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) PostLocation(w http.ResponseWriter, r *http.Request) {
+	var location models.LatLong
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	if err := r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := json.Unmarshal(body, &location); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println(location)
+
+	/*
+		out, err := json.MarshalIndent(resp, "", "     ")
+		if err != nil {
+			log.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	*/
 }
